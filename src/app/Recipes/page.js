8814bd709bce777f842a287c024cdeb5154f81e8
Mehrshad <ali@example.com>
2026-08-11 11:04:52 +0330

    fix recipes page build

--- src/app/Recipes/page.js
+++ src/app/Recipes/page.js
@@ -1,14 +1,18 @@
 import Header from "@/Components/Recipes/Header/Header";
 import ShowingRecipes from "@/Components/Recipes/main/ShowingRecipes";
 import SideBarRecipes from "@/Components/Recipes/main/SideBarRecipes";
+import Spinner from "@/UI/Spinner";
+import { Suspense } from "react";
 
 function page() {
     return (
         <div>
             <Header />
             <div className="mx-auto flex max-w-[1450px] gap-6 py-8">
-                <SideBarRecipes />
-                <ShowingRecipes />
+                <Suspense fallback={<Spinner />}>
+                    <ShowingRecipes />
+                    <SideBarRecipes />
+                </Suspense>
             </div>
         </div>
     );
